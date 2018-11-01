667c3ebcfa8e317cfa94d64ca64f25e9d90b63aa
Johannes Binder <j.binder.x@gmail.com>
2018-11-01 09:46:29 +0100

    Fix missing database in the db_use_utf8mb decorator

--- common/decorators/db_use_utf8mb.py
+++ common/decorators/db_use_utf8mb.py
@@ -3,13 +3,17 @@ import functools
 from common.utils.db_tools import use_mysql
 
 
-def db_use_utf8mb(f):
-    """ Sets the character set to utf8mb (MySQL only). Add after the db_session decorator. """
+def db_use_utf8mb(db):
+    """ Sets the character set to utf8mb (MySQL only). Add after the Pony db_session decorator. """
 
-    @functools.wraps(f)
-    def decorator(*args, **kw):
-        if use_mysql():
-            db.execute("SET NAMES utf8mb4;")
-        return f(*args, **kw)
+    def decorator_db_use_utf8mb(f):
+        @functools.wraps(f)
+        def decorator(*args, **kw):
+            if use_mysql():
+                # note: this must be done within a Pony session!
+                db.execute("SET NAMES utf8mb4;")
+            return f(*args, **kw)
 
-    return decorator
+        return decorator
+
+    return decorator_db_use_utf8mb
--- components/feedback/feedback_service.py
+++ components/feedback/feedback_service.py
@@ -2,21 +2,21 @@ from datetime import datetime
 
 from pony.orm import commit, db_session, select, ObjectNotFound
 
-from components.feedback.models import Feedback
+from components.feedback.models import Feedback, db
 from common.decorators.retry_on_error import retry_on_error
 from common.decorators.db_use_utf8mb import db_use_utf8mb
 
 
 class FeedbackService:
     @db_session
-    @db_use_utf8mb
+    @db_use_utf8mb(db)
     @retry_on_error
     def add(self, user_id, text):
         Feedback(user_id=user_id, text=text, created=datetime.utcnow())
         commit()
 
     @db_session
-    @db_use_utf8mb
+    @db_use_utf8mb(db)
     @retry_on_error
     def get(self, feedback_id):
         try:
@@ -25,21 +25,21 @@ class FeedbackService:
             return None
 
     @db_session
-    @db_use_utf8mb
+    @db_use_utf8mb(db)
     @retry_on_error
     def set_resolved(self, feedback_id):
         Feedback[feedback_id].done = datetime.utcnow()
         commit()
 
     @db_session
-    @db_use_utf8mb
+    @db_use_utf8mb(db)
     @retry_on_error
     def get_all(self):
         # noinspection PyTypeChecker
         return select(feedback for feedback in Feedback)[:]
 
     @db_session
-    @db_use_utf8mb
+    @db_use_utf8mb(db)
     @retry_on_error
     def get_stats(self):
         # noinspection PyTypeChecker
