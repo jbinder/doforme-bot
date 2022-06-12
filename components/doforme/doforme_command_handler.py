b9ed9b2bf005f0ef10f1e5052878b97f1c475984
Johannes Binder <j.binder.x@gmail.com>
2022-06-12 19:18:34 +0200

    Fix error handling when command is malformed

--- components/doforme/doforme_command_handler.py
+++ components/doforme/doforme_command_handler.py
@@ -54,6 +54,15 @@ class DoForMeCommandHandler(CommandHandlerBase):
         user_data['description'] = DoForMeCommandHandler._escape_text(_line_description)
         user_data['owner_id'] = update.effective_user.id
 
+        def parse_command(command):
+            match = re.match("(.*)@(.*)\s+in\s+(\d+)\s+(.*)", command)
+            if match is None:
+                return None
+            groups = match.groups()
+            if len(groups) != 4:
+                return None
+            return groups
+
         if not self.telegram_service.is_private_chat(update):
             user_data['chat_id'] = update.message.chat_id
 
@@ -62,15 +71,15 @@ class DoForMeCommandHandler(CommandHandlerBase):
             if mention is None:
                 # resolve user id by name
                 chat_users = self.telegram_service.get_chat_users(bot, user_data['chat_id'])
-                parsed_text = re.match("(.*)@(.*)\s+in\s+(\d+)\s+(.*)", text).groups()
-                user_name_parsed = parsed_text[1].strip()
-                user_data['user_id'] = next(
-                    (user_id for (user_id, user_name) in chat_users if user_name == '@' + user_name_parsed), 0)
-
-                if len(parsed_text) != 4 or not user_name_parsed:
+                user_name_parsed = None
+                parsed_text = parse_command(text)
+                if parsed_text is not None:
+                    user_name_parsed = parsed_text[1].strip()
+                    user_data['user_id'] = next(
+                        (user_id for (user_id, user_name) in chat_users if user_name == '@' + user_name_parsed), 0)
+                if not user_name_parsed:
                     self.telegram_service.send_reply(update.message, self.texts['help-do-group-format'])
                     return
-
                 if user_data['user_id'] == 0:
                     self.telegram_service.send_reply(update.message, self.texts['help-do-group-user-not-registered'](user_name_parsed))
                     return
@@ -78,12 +87,11 @@ class DoForMeCommandHandler(CommandHandlerBase):
                 # resolve user id using message metadata
                 text = update.message.text[:mention.offset] + '@' + update.message.text[mention.offset:]
                 text = text[len("/do"):].strip()
-                parsed_text = re.match("(.*)@(.*)\s+in\s+(\d+)\s+(.*)", text).groups()
+                parsed_text = parse_command(text)
+                if not parsed_text:
+                    self.telegram_service.send_reply(update.message, self.texts['help-do-group-format'])
+                    return
                 user_data['user_id'] = mention.user.id
-
-            if len(parsed_text) != 4:
-                self.telegram_service.send_reply(update.message, self.texts['help-do-group-format'])
-                return
 
             user_data['title'] = DoForMeCommandHandler._escape_text(parsed_text[0].strip())
 
